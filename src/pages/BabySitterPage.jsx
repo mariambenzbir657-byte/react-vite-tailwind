6fc52313a3f6f3abedcb1d7416dcd52330ee478e
Maraim Benzbir <maraimbenzbir657@gmail.com>
2026-02-09 00:03:32 +0100

    strcture projet

--- src/pages/BabySitterPage.jsx
+++
@@ -1,3 +0,0 @@
-export default function BabySitterPage() {
-    return <h1>Bienvenue BabySitter</h1>;
-}
--- src/pages/ParentPage.jsx
+++
@@ -1,3 +0,0 @@
-export default function ParentPage() {
-    return <h1>Bienvenue Parent</h1>;
-}
